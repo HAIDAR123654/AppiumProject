700f1aa5bc84ce596091d5595c745be0e1720047
HAIDAR123654 <haideralix22104@gmail.com>
2025-07-08 15:58:38 +0530

    Initial commit: Added appium sever

--- src/test/java/haidaraliappium/StartAppiumServer.java
+++ src/test/java/haidaraliappium/StartAppiumServer.java
@@ -22,7 +22,7 @@ public class StartAppiumServer {
 				.withIPAddress("192.168.0.113").usingPort(4723).build();
 		service2 = new AppiumServiceBuilder()
 				.withAppiumJS(new File("C:\\Users\\Asus\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-				.withIPAddress("192.168.0.113").usingPort(4725).build();
+				.withIPAddress("127.0.0.1").usingPort(4725).build();
 		service1.start();
 		service2.start();
 	}
@@ -30,6 +30,8 @@ public class StartAppiumServer {
 	@AfterSuite(alwaysRun = true)
 	public void stopServer() {
 		service1.stop();
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		service2.stop();
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 	}
 }
